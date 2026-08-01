7714ba6f0b7a505b2d7413dda3d57016c2f851b5
computerid-dev <nugrohokelyn@gmail.com>
2026-08-01 09:04:26 +0700

    Upload app/src/main/java/com/studymate/sm/cid/ui/theme/Color.kt

---
+++ app/src/main/java/com/studymate/sm/cid/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.studymate.sm.cid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Diambil dari palet logo Study Mate: navy gelap + hijau segar + aksen oranye
+val NavyDark = Color(0xFF1E3A5F)
+val NavyDeeper = Color(0xFF14283F)
+val GreenPrimary = Color(0xFF2E9E6D)
+val GreenLight = Color(0xFFDCF3E7)
+val AmberAccent = Color(0xFFF4A623)
+val SurfaceLight = Color(0xFFFAFAF8)
+val SurfaceDark = Color(0xFF11202F)
+val ErrorRed = Color(0xFFD64545)
